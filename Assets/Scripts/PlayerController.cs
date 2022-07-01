0ce362af4600603b88c19f4626e33fe26e869637
Diego Higuera <dhiguerafernandez@gmail.com>
2022-07-01 19:57:47 -0400

    project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 direction;
+    public float forwardSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);   
+    }
+}
